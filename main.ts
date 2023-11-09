bf4d3bb76b842d317d9517d1655775827f816bcb
Jacob Hummer <jcbhmr@outlook.com>
2023-11-09 14:09:38 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -24,52 +24,22 @@ const basicWrapper = (relative) => `\
 import { spawn } from "node:child_process";
 import { dirname, join } from "node:path";
 import { once } from "node:events";
-const file = join(dirname(process.argv[1]), ${JSON.stringify(relative)});
-const subprocess = spawn(file, { stdio: "inherit" });
+const __FILE = join(dirname(process.argv[1]), ${JSON.stringify(relative)});
+const subprocess = spawn(\`exec "$__FILE"\`, {
+  stdio: "inherit",
+  shell: "bash",
+  env: { ...process.env, __FILE },
+});
 process.exitCode = (await once(subprocess, "exit"))[0];
 `;
 
-const targetSpecificWrapper = ({
-  "windows-x64": windows_x64,
-  "linux-x64": linux_x64,
-  "macos-x64": macos_x64,
-}) => `\
+const targetSpecificWrapper = (map) => `\
 import { spawn } from "node:child_process";
 import { dirname, join } from "node:path";
 import { once } from "node:events";
-const relative = {
-  "win32,x64": ${JSON.stringify(windows_x64)},
-  "linux,x64": ${JSON.stringify(linux_x64)},
-  "darwin,x64": ${JSON.stringify(macos_x64)},
-}[[process.platform, process.arch].toString()];
+const key = \`\${process.env.RUNNER_OS.toLowerCase()}-\${process.env.RUNNER_ARCH.toLowerCase()}\`;
+const relative = ${JSON.stringify(map)}[key];
 const file = join(dirname(process.argv[1]), relative);
-const subprocess = spawn(file, { stdio: "inherit" });
-process.exitCode = (await once(subprocess, "exit"))[0];
-`;
-
-const targetSpecificGzipWrapper = ({
-  "windows-x64": windows_x64,
-  "linux-x64": linux_x64,
-  "macos-x64": macos_x64,
-}) => `\
-import { spawn } from "node:child_process";
-import { dirname, join } from "node:path";
-import { once } from "node:events";
-import { createReadStream, createWriteStream } from "node:fs";
-import { chmod, stat } from "node:fs/promises";
-import { pipeline } from "node:stream/promises";
-const relative = {
-  "win32,x64": ${JSON.stringify(windows_x64)},
-  "linux,x64": ${JSON.stringify(linux_x64)},
-  "darwin,x64": ${JSON.stringify(macos_x64)},
-}[[process.platform, process.arch].toString()];
-const file = join(dirname(process.argv[1]), relative);
-await pipeline(
-  createReadStream(\`\${file}.gz\`),
-  createGunzip(),
-  createWriteStream(file)
-);
-await chmod(file, (await stat(\`\${file}.gz\`)).mode);
 const subprocess = spawn(file, { stdio: "inherit" });
 process.exitCode = (await once(subprocess, "exit"))[0];
 `;
@@ -84,23 +54,15 @@ assert(action.runs.using === "executable", `unknown ${action.runs.using}`);
 action.runs.using = "node20";
 for (const stage of ["main", "pre", "post"]) {
   const entry = getStringOrMapInput(stage) || action.runs[stage];
-  let text: string;
-  if (typeof entry === "object") {
-    const relatives = Object.values(entry);
-    const absolutes = relatives.map((x) => join(root, x));
-    const sizes = await Promise.all(absolutes.map((x) => sizeMB(x)));
-    if (sizes.some((x) => x > 99)) {
-      await $`gzip ${absolutes.filter((x) => existsSync(x))}`;
-      text = targetSpecificGzipWrapper(entry);
-    } else {
+  if (entry) {
+    let text: string;
+    if (typeof entry === "object") {
       text = targetSpecificWrapper(entry);
+    } else {
+      text = basicWrapper(entry);
     }
-  } else if (typeof entry === "string") {
-    text = basicWrapper(entry);
-  } else {
-    continue;
+    action.runs[stage] = `_${stage}.mjs`;
+    await writeFile(join(root, action.runs[stage]), text);
   }
-  await writeFile(join(root, `_${stage}.mjs`), text);
-  action.runs[stage] = `_${stage}.mjs`;
 }
 await writeFile(actionPath, YAML.stringify(action));
