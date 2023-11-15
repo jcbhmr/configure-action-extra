1045127cff3d91cf8e6ea03eb72c162cb835f7d3
Jacob Hummer <jcbhmr@outlook.com>
2023-11-15 14:56:13 -0600

    save idea

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export async function needsGzip(path: string) {}
+
+export async function gzip(path: string) {}
+
+export async function readAction(root: string) {}
+
+export async function writeAction(root: string, action: any) {}
